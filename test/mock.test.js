dfc4412851ac981260d167db3d55a4c48e4bfa66
Simon David Ferreira Rodrigues <simon.rodrigues@glintt.com>
2021-01-29 15:54:11 +0000

    added mock test to .gitignore

--- test/mock.test.js
+++
@@ -1,9 +0,0 @@
-const { I, assert } = require("../lib")
-
-I("run a passing test", async () => {
-    return assert.equal(true, true)
-})
-
-I("run a failing test", async () => {
-    return assert.equal(true, false, `true does not equal false`)
-})
